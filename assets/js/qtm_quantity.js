92177986fecf6d52082fee2e0db6ddabbd6b39b9
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2025-07-23 23:45:52 +0600

    Cart Quantity

--- assets/js/qtm_quantity.js
+++ assets/js/qtm_quantity.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-
-})
-function data_quantity(){
-
+function triggerChange(input) {
+    const event = new Event('change', { bubbles: true });
+    input.dispatchEvent(event);
 }
-function qpm_increment( btn ){
+function qpm_increment(btn) {
     const parent = btn.closest('.quantity')
     let originQty = parent.querySelector('input.qty')
     let value = parseInt(originQty.value)
     // let addToCardBtn = parent.parentElement.querySelector('.add_to_cart_button ')
-    originQty.value = value +1;
+    originQty.value = value + 1;
     let addToCardBtn = parent.closest('.product')?.querySelector('.add_to_cart_button');
-    if(addToCardBtn){
+    if (addToCardBtn) {
         addToCardBtn.setAttribute('data-quantity', value + 1)
     }
+     triggerChange(originQty);
 }
-function qpm_decrement(btn){
+
+function qpm_decrement(btn) {
     const parent = btn.closest('.quantity')
     let originQty = parent.querySelector('input.qty')
     let value = parseInt(originQty.value)
-    let addToCardBtn = parent.parentElement.querySelector('.add_to_cart_button ')
-
-    if(addToCardBtn && value > 1){
-        originQty.value = value - 1
+    let addToCardBtn = parent.closest('.product')?.querySelector('.add_to_cart_button');
+    if (addToCardBtn){
         addToCardBtn.setAttribute('data-quantity', value - 1)
-
     }
+    if ( value > 1) {
+        originQty.value = value - 1
+    }
+    triggerChange(originQty);
+
 }
